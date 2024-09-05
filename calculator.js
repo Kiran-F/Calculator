d37213014d13b7578c52c9b82b4a14b4716293e1
Kiran Fatima <152872068+Kiran-F@users.noreply.github.com>
2024-09-05 21:30:39 +0500

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,15 @@
+// calculator program
+const display = document.querySelector("#display")
+function appendToDisplay(input){
+    display.value += input
+}
+function clearDisplay(){
+    display.value = ""
+}
+function calculate(){
+    try {
+        display.value = eval(display.value)
+    } catch (error) {
+        display.value = "Error"
+    }
+}
